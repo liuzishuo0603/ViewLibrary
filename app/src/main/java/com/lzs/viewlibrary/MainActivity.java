4c4b2dd2bb6ab452d167db4a123ae4b0c72192b6
liuzishuo0603 <705377068@qq.com>
2022-03-04 15:06:40 +0800

    gradle

--- app/src/main/java/com/lzs/viewlibrary/MainActivity.java
+++ app/src/main/java/com/lzs/viewlibrary/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.lzs.banner.BannerView;
+import com.lzs.rockerview.RootView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new BannerView().getBannerInfo();
+        new RootView().getRootInfo();
     }
 }
